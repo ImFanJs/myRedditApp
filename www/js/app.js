5a915a7e3207cc62b6b9a304f7e789fce55d5ece
Jesco Marketing <jesco.marketing@gmail.com>
2017-02-26 14:43:00 -0600

    Add link funcionality

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,9 @@
 (function() {
-  var app = angular.module('starter', ['ionic'])
+  var app = angular.module('starter', ['ionic', 'angularMoment'])
 
   app.controller('RedditCtrl', function($scope, $http) {
     $scope.posts = [];
-    $http.get('https://www.reddit.com/r/gaming/.json')
+    $http.get('https://www.reddit.com/r/gaming/new/.json')
       .success(function(posts) {
         // console.log(posts.data.children)
         angular.forEach(posts.data.children, function(post) {
@@ -11,6 +11,38 @@
           console.log(post);
         });
       });
+      $scope.cargarNuevosPosts = function() {
+        var params2 = {};
+        if($scope.posts.length > 0) {
+          params2['after'] = $scope.posts[$scope.posts.length - 1].name;
+        }
+        $http.get('https://www.reddit.com/r/gaming/new/.json', {params: params2})
+          .success(function(posts) {
+            // console.log(posts.data.children)
+            angular.forEach(posts.data.children, function(post) {
+              $scope.posts.push(post.data);
+              console.log(post);
+            });
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+          });
+      }
+      $scope.refrescar = function() {
+        var params2 = {'before': $scope.posts[0].name};
+        $http.get('https://www.reddit.com/r/gaming/new/.json', {params: params2})
+          .success(function(posts) {
+            var newPosts = [];
+            // console.log(posts.data.children)
+            angular.forEach(posts.data.children, function(post) {
+              newPosts.push(post.data);
+              console.log(post);
+            });
+            $scope.posts = newPosts.concat($scope.posts);
+            $scope.$broadcast('scroll.refreshComplete');
+          });
+      }
+      $scope.openLink = function(url) {
+          window.open(url, '_blank')
+      }
   });
 
   app.run(function($ionicPlatform) {
@@ -18,6 +50,9 @@
       if(window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.cordova && window.cordova.plugins.InAppBrowser) {
+        window.open = window.cordova.InAppBrowser.open;
       }
       if(window.StatusBar) {
         StatusBar.styleDefault();
